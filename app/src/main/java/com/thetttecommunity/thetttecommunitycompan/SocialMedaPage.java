17370be969f17f9fdea7a332ebb2cc6dab1ee846
sidahosa <shawnidahosa@yahoo.com>
2016-01-02 23:29:34 -0500

    Link the admin' social media page to the button

--- app/src/main/java/com/thetttecommunity/thetttecommunitycompan/SocialMedaPage.java
+++ app/src/main/java/com/thetttecommunity/thetttecommunitycompan/SocialMedaPage.java
@@ -1,31 +1,50 @@
 package com.thetttecommunity.thetttecommunitycompan;
 
-import android.graphics.Paint;
+
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.TextView;
+import android.widget.ImageView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.squareup.picasso.Picasso;
 
 public class SocialMedaPage extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_meda_page);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+
+        ImageView iv = (ImageView) findViewById(R.id.title_admins);
+        ImageView iv1 = (ImageView) findViewById(R.id.d1);
+        ImageView iv2 = (ImageView) findViewById(R.id.facebookDiesel);
+        ImageView iv3 = (ImageView) findViewById(R.id.twitterdiesel);
+        ImageView iv4 = (ImageView) findViewById(R.id.insta);
+        ImageView iv5 = (ImageView) findViewById(R.id.tumbler);
+
+        Picasso.with(this).load("http://i1036.photobucket.com/albums/a443" +
+                "/patsviper46/title_logo_zpsy36h8s7d.png").into(iv);
+        Picasso.with(this).load("http://i1036.photobucket.com/albums/a443/" +
+                "patsviper46/diesel_font_zpss9gp5gvn.png").into(iv1);
+        Picasso.with(this).load("http://i1036.photobucket.com/albums/a443/" +
+                "patsviper46/facebook_logo_zpshw1bp6mq.png").fit().centerCrop().into(iv2);
+        Picasso.with(this).load("http://i1036.photobucket.com/albums/a443/" +
+                "patsviper46/twitter_logo_zpswqppmpdv.png").fit().centerCrop().into(iv3);
+        Picasso.with(this).load("http://i1036.photobucket.com/albums/a443/" +
+                "patsviper46/instagram_zpssa270rmy.png").fit().centerCrop().into(iv4);
+        Picasso.with(this).load("http://i1036.photobucket.com/albums/a443/" +
+                "patsviper46/tumblr_logo_zpsdlktqs2n.png").fit().centerCrop().into(iv5);
+
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 }
