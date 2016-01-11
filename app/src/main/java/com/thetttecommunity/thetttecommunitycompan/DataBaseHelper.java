a016d5d76461d6bfc59fb518b42a1147ab9d6894
Ivan <ivanq247@gmail.com>
2016-01-11 18:25:58 -0500

    Fix grammer error as well as layout views.

--- app/src/main/java/com/thetttecommunity/thetttecommunitycompan/DataBaseHelper.java
+++ app/src/main/java/com/thetttecommunity/thetttecommunitycompan/DataBaseHelper.java
@@ -67,7 +67,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         List<Questions> quesList = new ArrayList<>();
 
         quesList.add(new Questions( "What was the one thing the Community did in order to keep the TAB project consistent?", "To use red James' instead of black ones", "To make Thomas the color green", "Make Gordon the color yellow and black", "Make Sidekickjason voice all the characters"));
-        quesList.add(new Questions( "Who is the former admin of the TTTE community?", "Ucwepn", "Wong Village", "Goku", "Gordon the Big Engine"));
+        quesList.add(new Questions( "Who was the former admin of the TTTE community?", "Ucwepn", "Wong Village", "Goku", "Gordon the Big Engine"));
         quesList.add(new Questions( "What was the first mini-remake uploaded to Youtube?", "Ghost Train/Percy's Ghostly Trick", "Thomas Gets Tricked/Thomas And Gordon", "Gordon Takes a Tumble", "Edward and The Mail"));
         quesList.add(new Questions( "Which two episodes were chosen for the second community project?", "Down The Mine and Off The Rails/Gordon Takes A Dip", "Percy’s Promise/Percy and The Signal", "The Special Letter/Gordon Goes Foreign", "The World's Strongest Engine/Henry’s Greatest Achievement"));
         quesList.add(new Questions( "What was the first remake submitted to the channel?", "Thomas And Gordon/Thomas Gets Tricked", "Down The Mine", "Thomas and The Stinky Cheese", "Gordon Goes Hollywood"));
@@ -81,7 +81,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         quesList.add(new Questions( "what is our official twitter name?", "@tttecommunity", "@thetttecommunity", "@gordondoesntgoforeign", "@toomanygordongoesforeignreference "));
         quesList.add(new Questions( "Who is the only admin to use Wooden trains?", "Enterprisingengine93", "DieselD199", "Percyengine619", "Sidekickjason"));
         quesList.add(new Questions( "The TTTE Community channel has all of the following social media except:", "Snapchat", "Facebook", "Twitter", "YouTube"));
-        quesList.add(new Questions( "What is the first community movie remake?", "The Adventure Begins", "Calling All Engines", "Hero of the Rails", "Gordon The Movie"));
+        quesList.add(new Questions( "What was the first community movie remake?", "The Adventure Begins", "Calling All Engines", "Hero of the Rails", "Gordon The Movie"));
 
         return quesList;
     }
