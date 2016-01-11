6af6fed00692ef6617c1217e324b7dca5e8f43eb
MABERCRO <mikeabo@verizon.net>
2016-01-11 15:43:33 -0500

    Class Deleted

--- app/src/main/java/com/thetttecommunity/thetttecommunitycompan/SocialMedaPage.java
+++ app/src/main/java/com/thetttecommunity/thetttecommunitycompan/SocialMedaPage.java
@@ -104,7 +104,7 @@ public class SocialMedaPage extends AppCompatActivity {
     //SKJ Social Media
 
     public void SKJFacebook(View view) {
-        goToUrl("https://www.facebook.com/sidekickjason/");
+        goToUrl("htps://www.facebook.com/sidekickjason/");
     }
     public void SKJTwitter(View view) {
         goToUrl("https://twitter.com/Sidekickjason");
