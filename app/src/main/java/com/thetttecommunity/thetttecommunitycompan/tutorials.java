7334176449c9ca82bcdf4c5ad138d59aadfaffbd
MABERCRO <mikeabo@verizon.net>
2016-01-11 13:37:32 -0500

    Added URLs to Tutorials

--- app/src/main/java/com/thetttecommunity/thetttecommunitycompan/tutorials.java
+++ app/src/main/java/com/thetttecommunity/thetttecommunitycompan/tutorials.java
@@ -1,25 +1,43 @@
 package com.thetttecommunity.thetttecommunitycompan;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
 public class tutorials extends AppCompatActivity {
 
+    Button tutorialButton1, tutorialButton2, tutorialButton3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorials);
 
+        tutorialButton1 = (Button) findViewById(R.id.button);
+        tutorialButton2 = (Button) findViewById(R.id.ytlink2);
+        tutorialButton2 = (Button) findViewById(R.id.ytlink3);
+
         AdView mAdView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+    }
+
+    public void gotoHowToRemakeClip (View view) { goToUrl("https://www.youtube.com/watch?v=CXQnNNqPjqk"); }
+
+    public void gotoAuditionToVoice (View view) { goToUrl("https://www.youtube.com/watch?v=as9SrhhGRsM"); }
+
+    public void gotoVotingRules (View view) { goToUrl("https://www.youtube.com/watch?v=mEl71nUUooM"); }
+
+    private void goToUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 
 }
