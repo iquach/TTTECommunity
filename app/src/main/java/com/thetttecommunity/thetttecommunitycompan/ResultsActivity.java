248f80e83ddc4b426ad83f1e7d15ca151e56ebb1
sidahosa <shawnidahosa@yahoo.com>
2016-01-15 22:43:39 -0500

    Final Version 2

--- app/src/main/java/com/thetttecommunity/thetttecommunitycompan/ResultsActivity.java
+++ app/src/main/java/com/thetttecommunity/thetttecommunitycompan/ResultsActivity.java
@@ -2,12 +2,16 @@ package com.thetttecommunity.thetttecommunitycompan;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class ResultsActivity extends Activity {
     @Override
@@ -30,6 +34,7 @@ public class ResultsActivity extends Activity {
         TextView user_score = (TextView) findViewById(R.id.score_user);
         //get text view
         TextView t=(TextView)findViewById(R.id.textResult);
+        ImageView iv1 = (ImageView)findViewById(R.id.prize);
         //get score
 
         Bundle b = getIntent().getExtras();
@@ -39,37 +44,85 @@ public class ResultsActivity extends Activity {
         switch (score)
         {
             case 0: t.setText("Yikes!!!");
+                Picasso.with(this).load("http://vignette4.wikia.nocookie.net/ttte/images/f/fd/" +
+                        "GordonTakesaTumble9.png/revision/latest?cb=20151113222820").fit().
+                        centerCrop().into(iv1);
                 break;
             case 1: t.setText("Bust My Buffers! Better Luck Next Time");
+                Picasso.with(this).load("http://vignette3.wikia.nocookie.net/ttte/images/6/69/" +
+                        "BufferBother56.JPG/revision/latest?cb=20151116224721").fit().
+                        centerCrop().into(iv1);
                 break;
             case 2: t.setText("Cinder and Ashes! Try again!");
+                Picasso.with(this).load("http://vignette4.wikia.nocookie.net/ttte/images/b/b5/" +
+                        "ThomasAndTheSpecialLetter37.png/revision/latest?cb=2" +
+                        "0150309184634").fit().centerCrop().into(iv1);
                 break;
             case 3: t.setText("Poor Henry!");
+                Picasso.with(this).load("http://vignette1.wikia.nocookie.net/ttte/images/0/05/Th" +
+                        "eFlyingKipper36.png/revision/latest" +
+                        "?cb=20160108174555").fit().centerCrop().into(iv1);
                 break;
             case 4: t.setText("O the Indignity!");
+                Picasso.with(this).load("http://vignette1.wikia.nocookie.net/ttte/image" +
+                        "s/e/ed/OfftheRails25.png/revision/la" +
+                        "test?cb=20131127173911").fit().centerCrop().into(iv1);
                 break;
             case 5: t.setText("You are causing confusion and delay with these wrong answers!");
+                Picasso.with(this).load("http://vignette3.wikia.nocookie.net/ttte/images/" +
+                        "c/cc/DuckTakesCharge52.png/revision/latest" +
+                        "?cb=20130219141552").fit().centerCrop().into(iv1);
                 break;
             case 6: t.setText("All you need is some determination....");
+                Picasso.with(this).load("http://vignette3.wikia.nocookie" +
+                        ".net/ttte/images/b/be/DuckTakesCharge31" +
+                        ".png/revision/latest?cb=20130218225736").fit().centerCrop().into(iv1);
                 break;
             case 7: t.setText("Flat My Funnel!");
+                Picasso.with(this).load("http://vignette1.wikia.nocookie.net/ttt" +
+                        "e/images/c/cf/SpecialFunnel2.png/revision/lates" +
+                        "t?cb=20140620201003").fit().centerCrop().into(iv1);
                 break;
             case 8: t.setText("There are two ways of doing things, the great western way " +
                     "or the wrong way. Choose Great Western");
+                Picasso.with(this).load("http://vignette2.wikia.nocookie.net/ttte/" +
+                        "images/d/df/DuckTakesCharge35.png/revision/latest" +
+                        "?cb=20130218225955").fit().centerCrop().into(iv1);
                 break;
             case 9: t.setText("You seem to be stuck on Gordon's Hill. You can do better.");
+                Picasso.with(this).load("http://vignette1.wikia.nocookie.net/ttte/" +
+                        "images/3/3a/EdwardandGordon8.png/revision/latest?cb=2" +
+                        "0121017225204").fit().centerCrop().into(iv1);
                 break;
             case 10: t.setText("At least you are over Gordon's Hill. Good Work.");
+                Picasso.with(this).load("http://vignette2.wikia.nocookie.net/ttte/images/e/e7/" +
+                        "TheTroublewithMud76.png/revisi" +
+                        "on/latest?cb=20140407084653").fit().centerCrop().into(iv1);
                 break;
             case 11: t.setText("WELL DONE TANKIE, WELL DONE!");
+                Picasso.with(this).load("http://vignette4.wikia.nocookie.net/" +
+                        "ttte/images/f/f5/ThomasAndTheMagicRailroad968." +
+                        "png/revision/latest?cb=20150728162529").fit().centerCrop().into(iv1);
                 break;
             case 12: t.setText("The Fat Controller is Proud! He may give you a branch line.");
+                Picasso.with(this).load("http://vignette3.wikia.nocookie.net/ttte/" +
+                        "images/a/a5/Thomas%2CPercyandtheDragon7.PNG/" +
+                        "revision/latest?cb=20130521124746").fit().centerCrop().into(iv1);
                 break;
             case 13: t.setText("Your reward is a new coat a paint!");
+                Picasso.with(this).load("http://vignette3.wikia.nocookie.net/ttte/" +
+                        "images/e/e9/EdwardandGordon44.png/" +
+                        "revision/latest?cb=20121017232553").fit().centerCrop().into(iv1);
                 break;
             case 14:t.setText("The Fat Controller is giving you special coaches!");
+                Picasso.with(this).load("http://vignette2.wikia.nocookie.net/" +
+                        "ttte/images/f/f0/ThomasandtheBreakdownTrain46." +
+                        "png/revision/latest?cb=20131117163450").fit().centerCrop().into(iv1);
                 break;
             case 15:t.setText("You are a really useful engine!");
+                Picasso.with(this).load("http://vignette3.wikia.nocookie.net/" +
+                        "ttte/images/5/50/ThomasandGordon63.png/revision/" +
+                        "latest?cb=20121017173537").fit().centerCrop().into(iv1);
                 break;
         }
     }
